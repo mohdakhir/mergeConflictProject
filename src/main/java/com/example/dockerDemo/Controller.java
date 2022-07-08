ab7c6c04eadc4ecfad630ffedee9d5586e96a19e
Mohd Akhir <74229332+mohdakhir@users.noreply.github.com>
2022-07-08 17:28:34 +0530

    second commit

--- src/main/java/com/example/dockerDemo/Controller.java
+++ src/main/java/com/example/dockerDemo/Controller.java
@@ -1,13 +1,16 @@
 package com.example.dockerDemo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/v1")
 public class Controller {
     @GetMapping("/get")
     public String getString()
     {
         return "hello docker";
     }
+    
 }
