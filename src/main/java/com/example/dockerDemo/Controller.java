8867922ef3fc2a94738ddf2049b2ce851f3455e2
Mohd Akhir <74229332+mohdakhir@users.noreply.github.com>
2022-07-08 18:17:39 +0530

    third commit

--- src/main/java/com/example/dockerDemo/Controller.java
+++ src/main/java/com/example/dockerDemo/Controller.java
@@ -1,9 +1,12 @@
 package com.example.dockerDemo;
 
+import org.apache.logging.log4j.status.StatusData;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @RequestMapping("/v1")
@@ -13,7 +16,11 @@ public class Controller {
     {
         return "hello docker";
     }
-
+    @GetMapping(value="path")
+    public String getMethodName(@RequestParam String param) {
+        return "akhir";
+    }
+    
     
     
 }
